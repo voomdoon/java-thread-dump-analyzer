c068e9123f17651ce3b827a8b7ac112dc0bd0671
voomdoon <nilreb24-github20141105@yahoo.de>
2024-02-28 11:48:32 +0100

    refactor: add main draft

---
+++ src/main/java/de/voomdoon/tool/java/threaddump/analyzer/JavaThreadDumpAnalyzerMain.java
@@ -0,0 +1,22 @@
+package de.voomdoon.tool.java.threaddump.analyzer;
+
+/**
+ * DOCME add JavaDoc for
+ *
+ * @author André Schulz
+ *
+ * @since 0.1.0
+ */
+public class JavaThreadDumpAnalyzerMain {
+
+	/**
+	 * DOCME add JavaDoc for method main
+	 * 
+	 * @param args
+	 * @since 0.1.0
+	 */
+	public static void main(String[] args) {
+		// TODO implement main
+		throw new UnsupportedOperationException("Method 'main' not implemented yet");
+	}
+}
